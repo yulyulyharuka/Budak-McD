6c3a57a4b91758569dafd19ff98b80f1f6fd5542
mnurraihann <raihannaf@gmail.com>
2018-11-24 21:42:31 +0700

    load gnb model

--- coba.py
+++ coba.py
@@ -3,53 +3,37 @@ import json
 import pandas as pd
 from sklearn import metrics
 from sklearn.naive_bayes import GaussianNB
+from sklearn.externals import joblib
 
-# Reading Train Data
-
-heartD = pd.read_csv('tubes2_HeartDisease_train.csv')
-heartD_target = heartD['Column14']
-heartD_data = heartD.loc[:, :'Column13']
-heartD_feature_names = ['age', 'sex', 'chest-pain type', 'resting blod presure', 'serum cholestrol', 
-                        'fasting blood sugar above 120 mg/dl', 'resting ECG', 'max heart rate achieved', 'exercise induced angina',
-                       'ST depression induced', 'peak exercise ST segment', 'member of major vessel', 'thal']
-
-heartD_target_names = ['absence', 'presence', 'presence', 'presence', 'presence']
-
-# # fit a Naive Bayes model to the data
-# gnb = GaussianNB()
-# gnb_class = gnb.fit(heartD_data, heartD_target)
-# print(gnb_class)
-
-# # make predictions
-# expected = heartD_target
-# predicted = gnb_class.predict(testD)
-# # summarize the fit of the model
+#Load model
+gnb_load = joblib.load('gnb_HeartDisease_model.joblib')
 
 app = Flask(__name__)
 
 @app.route('/index.html')
 def index():
-   return render_template('index.html')
+   return render_template('index')
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
-    result = []
+    data = []
     if request.method == 'POST':
-        patient_age = request.form['age']
-        patient_sex = request.form['sex']
-        patient_cpt = request.form['cpt']
-        patient_rbp = request.form['rbp']
-        patient_sc = request.form['sc']
-        patient_fbs = request.form['fbs']
-        patient_re = request.form['re']
-        patient_mhra = request.form['mhra']
-        patient_eia = request.form['eia']
-        patient_std = request.form['std']
-        patient_pess = request.form['pess']
-        patient_mvcf = request.form['mvcf']
-        patient_thal = request.form['thal']
-        result = [patient_age, patient_sex, patient_cpt, patient_rbp, patient_sc, patient_fbs, patient_re, patient_mhra, patient_eia, patient_std, patient_pess, patient_mvcf, patient_thal]
-        return render_template("result.html",result = result)
+        patient_age = int(request.form['age'])
+        patient_sex = int(request.form['sex'])
+        patient_cpt = int(request.form['cpt'])
+        patient_rbp = int(request.form['rbp'])
+        patient_sc = int(request.form['sc'])
+        patient_fbs = int(request.form['fbs'])
+        patient_re = int(request.form['re'])
+        patient_mhra = int(request.form['mhra'])
+        patient_eia = int(request.form['eia'])
+        patient_std = int(request.form['std'])
+        patient_pess = int(request.form['pess'])
+        patient_mvcf = int(request.form['mvcf'])
+        patient_thal = int(request.form['thal'])
+        data = [patient_age, patient_sex, patient_cpt, patient_rbp, patient_sc, patient_fbs, patient_re, patient_mhra, patient_eia, patient_std, patient_pess, patient_mvcf, patient_thal]
+        result = gnb_load.predict([data])
+        return render_template("result.html",result = str(result[0]))
 
 if __name__ == '__main__':
    app.run(debug = True)
