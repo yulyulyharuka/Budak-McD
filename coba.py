5f2e0e946a5fabda703c22d07347aff3bf01e1ad
yulyulyharuka <yulyharuka@gmail.com>
2018-11-24 16:44:08 +0700

    show result form in result.html

---
+++ coba.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/index.html')
+def index():
+   return render_template('index.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
+
+if __name__ == '__main__':
+   app.run(debug = True)
